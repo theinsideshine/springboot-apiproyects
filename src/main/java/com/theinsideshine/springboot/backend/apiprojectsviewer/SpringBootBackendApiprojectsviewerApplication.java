d9aef040ac459a0c5f4b978aae1794fc669e0794
theinsideshine <educacion.ta@gmail.com>
2022-06-17 07:54:45 -0300

    Comienzo del proyecto

---
+++ src/main/java/com/theinsideshine/springboot/backend/apiprojectsviewer/SpringBootBackendApiprojectsviewerApplication.java
@@ -0,0 +1,13 @@
+package com.theinsideshine.springboot.backend.apiprojectsviewer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBackendApiprojectsviewerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBackendApiprojectsviewerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/theinsideshine/springboot/backend/apiprojectsviewer/SpringBootBackendApiprojectsviewerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.theinsideshine.springboot.backend.apiprojectsviewer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBackendApiprojectsviewerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
